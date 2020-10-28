1c76d17ea776f5994f5c565a3b47e12297dc9ebf
BlainWu <896511142@qq.com>
2020-10-28 16:22:37 +0800

    Add files via upload

---
+++ uart.c
@@ -0,0 +1,7 @@
+//===============================
+//Author: Peilin Wu
+//Data: 28/10/20
+//Descibtion: UART driver source file
+//Student Number: 17049125
+//E-mail: p.wu@northumbria.ac.uk
+//===============================
