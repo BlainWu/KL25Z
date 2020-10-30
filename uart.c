801be08870539a5339e29fd26b3ec923d36a88c0
BlainWu <896511142@qq.com>
2020-10-30 20:56:33 +0800

    Add files via upload

--- uart.c
+++ uart.c
@@ -9,35 +9,81 @@
 # include "uart.h"
 
 
-void uart_init(uint8_t UART_No,uint16_t baud_rate){
+void uart0_init(uint16_t baud_rate){
 	
-	if(UART_No < 0 || UART_No > 2){
-		return;																	//Check the port number
-	}
+	SIM_SOPT2 |= SIM_SOPT2_UART0SRC(0x01);	//Set MCGFLLCLK as clock, 20.97Mhz
+	SIM_SCGC4 |= SIM_SCGC4_UART0(0x1);			//Enable UART0 clock
+	SIM_SCGC5 |= SIM_SCGC5_PORTA(0x1);
 	
-	//UART0
-	if(UART_No == 0 ){
-
-		SIM_SOPT2 |= SIM_SOPT2_UART0SRC(0x01);	//Set MCGFLLCLK as clock, 20.97Mhz
-		SIM_SCGC4 |= SIM_SCGC4_UART0(0x1);			//Enable UART0 clock
-		SIM_SCGC5 |= SIM_SCGC5_PORTA(0x1);
-		
-		PORTA_PCR1 |= PORT_PCR_MUX(0x02);
-		PORTA_PCR2 |= PORT_PCR_MUX(0x02);				//Make PTA1->RX PTA2->TX ;
-		
-		UART0->C2 = 0; 													//Turn off uart0 before config
-		
-		uint16_t sbr = 20970000/(16 * baud_rate);//Calculate SBR
-		UART0_BDH |= UART0_BDH_LBKDIE(0x0);
-		UART0_BDH |= UART0_BDH_RXEDGIE(0x0);
-		UART0_BDH |= UART0_BDH_SBNS(0x0);
-		UART0_BDH |= (UART0_BDH_SBR_MASK & (sbr>>8));
-		UART0_BDL = (uint8_t)(sbr & UART0_BDL_SBR_MASK); //Set Baud rate
-		
-		UART0_C1 = 0x00; 													//8-bit ,noparity
-		UART0_C4 |= UART0_C4_OSR(0xF);						//over-sampling-ratio = 16
-		UART0_C2 |= UART0_C2_TE(0x01);						//Transmit enable
-		UART0_C2 |= UART0_C2_RE(0x01);						//Receiver enable
-	}
+	PORTA_PCR1 |= PORT_PCR_MUX(0x02);
+	PORTA_PCR2 |= PORT_PCR_MUX(0x02);				//Make PTA1->RX PTA2->TX ;
+	
+	UART0->C2 = 0; 													//Turn off uart0 before config
+	
+	uint16_t sbr = 20970000/(16 * baud_rate);//Calculate SBR
+	
+	UART0_BDH |= UART0_BDH_LBKDIE(0x0);
+	UART0_BDH |= UART0_BDH_RXEDGIE(0x0);
+	UART0_BDH |= UART0_BDH_SBNS(0x0);
+	UART0_BDH |= (UART0_BDH_SBR_MASK & (sbr>>8));
+	UART0_BDL = (uint8_t)(sbr & UART0_BDL_SBR_MASK); //Set Baud rate
+	
+	UART0_C1 = 0x00; 													//8-bit ,noparity
+	UART0_C4 |= UART0_C4_OSR(0xF);						//over-sampling-ratio = 16
+	UART0_C2 |= UART0_C2_TE(0x01);						//Transmit enable
+	UART0_C2 |= UART0_C2_RE(0x01);						//Receiver enable
 }
+
+
+
+uint8_t uart0_sent_char(uint8_t ch){
+		
+	uint32_t i;
+	
+	for(i = 0 ; i < 0xffff ; i++){
+		if(UART0_S1 & UART0_S1_TDRE_MASK){				//if Transmit Data Reg is Empty
+			UART0_D = ch;
+			break;																	//break the check loop
+		}
+	}
+	if(i >= 0xffff){														//fail to sent the char
+		return 0;
+	}
+	else
+		return 1;																	//sent successfully
+
+}
+
+uint8_t uart0_sent_string(char* str){
+	
+	uint8_t i ;
+	
+	for(i = 0; str[i] != '\0' ; i++)
+	{
+		if(!uart0_sent_char(str[i]))
+			return 0;
+	}
+	
+	return 1;
+
+}
+
+uint8_t uart0_reci_char(uint8_t* trigger){
+	uint32_t i;
+	uint8_t  data;
+
+	for (i = 0; i < 0xFBBB; i++){
+			if (UART0_S1 & UART0_S1_RDRF_MASK){			//iF Reciver Data Reg is Full
+					data = UART0_D;
+					*trigger = 1;												//Show recive a char
+					break;
+			}
+	}
+	if(i >= 0xFBBB){
+			data = 0xFF; 
+			*trigger = 0;    												//Did not recive a char
+	}
+	return data;
+}
+
 
--- uart.h
+++ uart.h
@@ -10,19 +10,45 @@
 # define _UART_H
 
 #include "MKL25Z4.h"
-# define UART_0 0
-# define UART_1 1
-# define UART_2 2
 
 
 //===========================================
 //Function name: uart_init
 //Describtion: Initialize the UART
-//Parameters: UART_No: UART_0/UART_1/UART_2
-//						baud_rate:1200/2400/4800/9600/38400/575600/115200/460800/921600/230400
+//Parameters:  baud_rate:1200/2400/4800/9600/38400/575600/115200/460800/921600/230400
 //Function return: None
 //===========================================
-void uart_init(uint8_t UART_No,uint16_t baud_rate);
+void uart0_init(uint16_t baud_rate);
+
+
+//===========================================
+//Function name: uart_sent_char
+//Describtion: 			Sent one character
+//Parameters:				ch: The charater which is sent
+//Function return: 1 -> sent successfully
+//								 0 -> fail to sent
+//===========================================
+uint8_t uart0_sent_char(uint8_t ch);
+
+
+//===========================================
+//Function name: uart0_sent_string
+//Describtion: 			Sent a string
+//Parameters:				ch: The charater which is sent
+//Function return: 1 -> sent successfully
+//								 0 -> fail to sent
+//===========================================
+uint8_t uart0_sent_string(char* str);
+
+
+//===========================================
+//Function name: uart0_reci_char
+//Describtion: 			Recive a character
+//Parameters:				trigger->when recived a character,trigger=1; else trigger=0
+//Function return: 	return a char(8bit)
+//===========================================
+uint8_t uart0_reci_char(uint8_t* trigger);
+
 
 
 # endif
