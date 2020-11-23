60304278852adb20b77a9e403c4f87d7fa588d17
BlainWu <896511142@qq.com>
2020-11-23 19:57:34 +0800

    Add files via upload

---
+++ pwmled.c
@@ -0,0 +1,49 @@
+#include "pwmled.h"
+#define MOD_VALUE 9999
+
+void pwmled_init(){
+	SIM_SCGC5 |= SIM_SCGC5_PORTB(1) + SIM_SCGC5_PORTD(1);	//enble portB portD
+	SIM_SCGC6 |= SIM_SCGC6_TPM0(1) + SIM_SCGC6_TPM2(1);		//enable TPM0 TPM2
+	SIM_SOPT2 |= SIM_SOPT2_TPMSRC(1);											//Set MCGFLLCLK as TPM clock, 20.97Mhz
+	
+	PORTD_PCR1 |= PORT_PCR_MUX(4);	//set PORTD1 I/O as TPM0_CH0
+	PORTB_PCR18 |=PORT_PCR_MUX(3);	//set PORTB18 I/0 as TPM2_CH0
+	PORTB_PCR19 |=PORT_PCR_MUX(3);	//set PORTB19 I/0 as TPM2_CH1
+	
+	//set as High-Ture pulses;Edge-aligned PWM
+	//Blue
+	TPM0_SC = 0;
+	TPM0->CONTROLS[1].CnSC = 0x24;
+	TPM0->MOD = MOD_VALUE;
+	TPM0->CONTROLS[1].CnV = 1;
+	TPM0_SC = 0x08;
+
+	//Red Green
+	TPM2->SC = 0;
+	TPM2->CONTROLS[0].CnSC = 0x24;
+	TPM2->CONTROLS[1].CnSC = 0x24;
+	TPM2->MOD = MOD_VALUE;
+	TPM2->CONTROLS[0].CnV = 1;
+	TPM2->CONTROLS[1].CnV = 1;
+	TPM2->SC = 0x08;
+}
+
+void pwmled_set(uint8_t duty_cycle,uint8_t colour){
+	uint8_t CnV_VALUE = duty_cycle * (MOD_VALUE+1) / 100;
+	switch(colour){
+		case 'R':
+		case 'r':
+			TPM2->CONTROLS[0].CnV = CnV_VALUE;
+			break;
+		
+		case 'G':
+		case 'g':
+			TPM2->CONTROLS[1].CnV = CnV_VALUE;
+			break;
+		
+		case 'B':
+		case 'b':
+			TPM0->CONTROLS[1].CnV = CnV_VALUE;
+			break;
+	}
+}
---
+++ pwmled.h
@@ -0,0 +1,9 @@
+#ifndef _PWMLED_H
+#define _PWMLED_H
+
+#include "MKL25Z4.h"
+
+void pwmled_init();
+void pwmled_set(uint8_t duty_cycle,uint8_t colour);
+
+#endif
